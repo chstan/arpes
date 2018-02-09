3d115d42ab0d84e70e19808d1f81210149bd3f19
Conrad Stansbury <chstansbury@gmail.com>
2018-02-09 15:47:28 -0800

    Make basic plotting accept datasets

--- arpes/plotting/basic.py
+++ arpes/plotting/basic.py
@@ -1,30 +1,48 @@
+import warnings
+
 import pandas as pd
 from arpes.utilities import default_dataset
 from arpes.preparation import normalize_dim
 from arpes.io import load_dataset
 from arpes.pipelines import convert_scan_to_kspace
 
+import xarray as xr
+
 __all__ = ['make_reference_plots']
+
 
 def make_reference_plots(df: pd.DataFrame=None, with_kspace=False):
     if df is None:
         df = default_dataset()
 
-    df = df[df.spectrum_type != 'xps_spectrum']
+    try:
+        df = df[df.spectrum_type != 'xps_spectrum']
+    except TypeError:
+        warnings.warn('Unable to filter out XPS files, did you attach spectra type?')
 
     # Make scans indicating cut locations
     for index, row in df.iterrows():
-        scan = load_dataset(row.id, df)
-        scan.S.reference_plot(out=True, use_id=False)
+        try:
+            scan = load_dataset(row.id, df)
 
-        if scan.S.spectrum_type == 'spectrum':
-            # Also go and make a normalized version
-            normed = normalize_dim(scan, 'phi')
-            normed.S.reference_plot(out=True, use_id=False, pattern='{}_norm_phi.png')
+            if isinstance(scan, xr.Dataset):
+                # make plot series normalized by current:
+                scan.S.reference_plot(out=True)
+            else:
+                scan.S.reference_plot(out=True, use_id=False)
 
-            if with_kspace:
-                kspace_converted = convert_scan_to_kspace(scan)
-                kspace_converted.S.reference_plot(out=True, use_id=False, pattern='k_{}.png')
+                if scan.S.spectrum_type == 'spectrum':
+                    # Also go and make a normalized version
+                    normed = normalize_dim(scan, 'phi')
+                    normed.S.reference_plot(out=True, use_id=False, pattern='{}_norm_phi.png')
 
-                normed_k = normalize_dim(kspace_converted, 'kp')
-                normed_k.S.reference_plot(out=True, use_id=False, pattern='k_{}_norm_kp.png')
+                    if with_kspace:
+                        kspace_converted = convert_scan_to_kspace(scan)
+                        kspace_converted.S.reference_plot(out=True, use_id=False, pattern='k_{}.png')
+
+                        normed_k = normalize_dim(kspace_converted, 'kp')
+                        normed_k.S.reference_plot(out=True, use_id=False, pattern='k_{}_norm_kp.png')
+
+        except Exception as e:
+            print(str(e))
+            warnings.warn('Cannot make plots for {}'.format(index))
