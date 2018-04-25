33a6d05da6e51f32ac52ab7126c4c1bb55fadaf1
Conrad Stansbury <chstansbury@gmail.com>
2018-04-25 09:28:21 -0700

    More helpful example

--- example_configuration/local_config.example.py
+++ example_configuration/local_config.example.py
@@ -1,2 +1,10 @@
-DATA_PATH = '/Users/chstansbury/Research/lanzara/data/'
-DATASET_CACHE_PATH = '/Users/chstansbury/Research/lanzara/data/cache/'
+import os
+
+# note to Windows users: you need to specify your paths Windows style, this looks like:
+# r'C:\Users\user\Documents\wherever'
+# The r is for escaping the string, you can also do, if you like:
+# 'C:\\Users\\user\\Documents\\wherever'
+os.environ['ARPES_ROOT'] = r'/Users/chstansbury/PycharmProjects/python-arpes/' # <- change this out
+
+DATA_PATH = None
+# DATA_PATH = '/Users/chstansbury/Research/lanzara/data/' # <- change me too
