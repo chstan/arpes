6124156f6de8727e44189a876632140c7a104b7a
Conrad Stansbury <chstansbury@gmail.com>
2018-04-30 20:40:34 -0700

    Load settings from interactive, CursorTool

--- arpes/plotting/interactive_utils.py
+++ arpes/plotting/interactive_utils.py
@@ -2,8 +2,11 @@ import warnings
 
 import numpy as np
 import xarray as xr
+import colorcet as cc
+from bokeh import palettes
 from bokeh.application import Application
 from bokeh.application.handlers import FunctionHandler
+from bokeh.models.widgets.markups import Div
 from bokeh.io import show, output_notebook
 
 from abc import ABC, abstractmethod
@@ -14,7 +17,66 @@ import arpes.config
 from arpes.io import load_dataset
 from typing import Union
 
-__all__ = ('BokehInteractiveTool',)
+__all__ = ('BokehInteractiveTool', 'CursorTool',)
+
+class CursorTool(object):
+    _cursor = None
+    _cursor_info = None
+    _horiz_cursor_x = None
+    _horiz_cursor_y = None
+    _vert_cursor_x = None
+    _vert_cursor_y = None
+    _cursor_lines = None
+    _cursor_dims = None
+
+    @property
+    def cursor_dims(self):
+        return self._cursor_dims
+
+    @cursor_dims.setter
+    def cursor_dims(self, value):
+        self._cursor_dims = value
+
+    @property
+    def cursor_dict(self):
+        if self._cursor_dims is None:
+            return None
+
+        return dict(zip(self.cursor_dims, self.cursor))
+
+    @property
+    def cursor(self):
+        return self._cursor
+
+    def add_cursor_lines(self, figure):
+        cursor_lines =  figure.multi_line(xs=[self._horiz_cursor_x, self._vert_cursor_x],
+                                          ys=[self._horiz_cursor_y, self._vert_cursor_y],
+                                          line_color='white', line_width=2, line_dash='dotted')
+        self._cursor_lines = cursor_lines
+        return cursor_lines
+
+    @cursor.setter
+    def cursor(self, values):
+        self._cursor = values
+        if self._cursor_info is None:
+            self._cursor_info = Div(text='')
+            self._horiz_cursor_x = list(self.data_range['x'])
+            self._horiz_cursor_y = [0, 0]
+            self._vert_cursor_x = [0, 0]
+            self._vert_cursor_y = list(self.data_range['y'])
+        else:
+            self._vert_cursor_x[0] = self._vert_cursor_x[1] = self.cursor[0]
+            self._horiz_cursor_y[0] = self._horiz_cursor_y[1] = self.cursor[1]
+
+        self._cursor_info.text = '<h2>Cursor:</h2><span>({})</span>'.format(
+            ', '.join("{0:.3f}".format(c) for c in self.cursor))
+
+        if self._cursor_lines is not None:
+            self._cursor_lines.data_source.data = {
+                'xs': [self._horiz_cursor_x, self._vert_cursor_x],
+                'ys': [self._horiz_cursor_y, self._vert_cursor_y],
+            }
+
 
 class BokehInteractiveTool(ABC):
     auto_rebin = True
@@ -33,7 +95,24 @@ class BokehInteractiveTool(ABC):
             # according to
             # https://github.com/bokeh/bokeh/blob/0.12.10/examples/howto/server_embed/notebook_embed.ipynb
 
+    def load_settings(self, **kwargs):
+        self.settings = arpes.config.SETTINGS.get('interactive', {}).copy()
+        for k, v in kwargs.items():
+            if k not in self.settings:
+                self.settings[k] = v
+
+    @property
+    def default_palette(self):
+        palette_options = {
+            'viridis': palettes.viridis(256),
+            'magma': palettes.magma(256),
+            'coolwarm': cc.coolwarm,
+        }
+
+        return palette_options[self.settings.get('palette', 'viridis')]
+
     def __init__(self):
+        self.settings = None
         self.app_context = {
             'data': None,
             'plots': {},
@@ -44,17 +123,17 @@ class BokehInteractiveTool(ABC):
 
         self.init_bokeh_server()
 
-    def __getattr__(self, item):
+    def __getattribute__(self, item):
         """
         Allow more convenient use of attributes from self.app_context. This is a bit strange.
         :param item:
         :return:
         """
-        if item in self.app_context:
-            return self.app_context[item]
-
-        return getattr(self, item)
-
+        try:
+            return super().__getattribute__(item)
+        except AttributeError:
+            if item in self.app_context:
+                return self.app_context[item]
 
     @abstractmethod
     def tool_handler(self, doc):
