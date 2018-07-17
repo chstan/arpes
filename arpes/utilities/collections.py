c4583f0c0fcfe1b495c14f5e7a566a38b3a55111
Conrad Stansbury <chstansbury@gmail.com>
2018-07-17 14:23:31 -0700

    Added utilities for collection equality

---
+++ arpes/utilities/collections.py
@@ -0,0 +1,50 @@
+__all__ = ('deep_equals',)
+
+
+def deep_equals(a, b):
+    if not isinstance(b, type(a)):
+        return False
+
+    if isinstance(a, (int, str, float,),):
+        return a == b
+
+    if a is None:
+        return b is None
+
+    if not isinstance(a, (dict, list, tuple, set,)):
+        raise TypeError('Only dict, list, tuple, and set are supported by deep_equals, not {}'.format(type(a)))
+
+    if isinstance(a, set):
+        for item in a:
+            if item not in b:
+                return False
+
+        for item in b:
+            if item not in a:
+                return False
+
+        return True
+
+    if isinstance(a, (list, tuple)):
+        if len(a) != len(b):
+            return False
+
+        for i in range(len(a)):
+            item_a, item_b = a[i], b[i]
+
+            if not deep_equals(item_a, item_b):
+                return False
+
+        return True
+
+    if isinstance(a, dict):
+        if not set(a.keys()) == set(b.keys()):
+            return False
+
+        for k in a.keys():
+            item_a, item_b = a[k], b[k]
+
+            if not deep_equals(item_a, item_b):
+                return False
+
+        return True
