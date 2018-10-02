0407c78ca062fb85296f2fa5885db781653bcc53
Conrad Stansbury <chstansbury@gmail.com>
2018-10-02 09:46:16 -0700

    Make negate_energy safer so that it can be automatically called
    Should probably deprecate this by a function that will swap all axes
    found to be opposite

--- arpes/repair.py
+++ arpes/repair.py
@@ -3,5 +3,6 @@ from arpes.typing import DataType
 __all__ = ('negate_energy',)
 
 def negate_energy(data: DataType):
-    data.coords['eV'].values = -data.coords['eV'].values
+    if 'eV' in data.coords:
+        data.coords['eV'].values = -data.coords['eV'].values
     return data
