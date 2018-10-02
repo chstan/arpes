35a41273a828a16ccd16e1e546627d9dc7370e64
Conrad Stansbury <chstansbury@gmail.com>
2018-10-02 09:44:09 -0700

    Allow fitting composite models in broadcast

--- arpes/fits/utilities.py
+++ arpes/fits/utilities.py
@@ -1,5 +1,10 @@
+import functools
+from string import ascii_lowercase
+import operator
 import numpy as np
 import xarray as xr
+
+import typing
 from arpes.typing import DataType
 from tqdm import tqdm_notebook
 
@@ -8,7 +13,10 @@ from arpes.utilities import normalize_to_spectrum
 __all__ = ('broadcast_model',)
 
 
-def broadcast_model(model_cls: type, data: DataType, broadcast_dims, progress=True, constraints=None, dataset=True):
+TypeIterable = typing.Union[typing.List[type], typing.Tuple[type]]
+
+def broadcast_model(model_cls: typing.Union[type, TypeIterable],
+                    data: DataType, broadcast_dims, constraints=None, progress=True, dataset=True):
     if constraints is None:
         constraints = {}
 
@@ -27,7 +35,20 @@ def broadcast_model(model_cls: type, data: DataType, broadcast_dims, progress=Tr
     residual = data.copy(deep=True)
     residual.values = np.zeros(residual.shape)
 
-    model = model_cls()
+    new_params = None
+    if isinstance(model_cls, (list, tuple)):
+        models = [m(prefix='{}_'.format(ascii_lowercase[i])) for i, m in enumerate(model_cls)]
+        if isinstance(constraints, (list, tuple)):
+            for cs, m in zip(constraints, models):
+                for name, constraints_for_name in cs.items():
+                    m.set_param_hint(name, **constraints_for_name)
+
+            constraints = {}
+
+        model = functools.reduce(operator.add, models)
+        new_params = model.make_params()
+    else:
+        model = model_cls()
 
     n_fits = np.prod(np.array(list(template.S.dshape.values())))
     wrap_progress = lambda x, *args, **kwargs: x
@@ -38,8 +59,9 @@ def broadcast_model(model_cls: type, data: DataType, broadcast_dims, progress=Tr
                                              total=n_fits):
         cut_data = data.sel(**cut_coords)
         fit_result = model.guess_fit(cut_data, params=constraints)
+
         template.loc[cut_coords] = fit_result
-        residual.loc[cut_coords] = fit_result.residual
+        residual.loc[cut_coords] = fit_result.residual if fit_result is not None else None
 
     if dataset:
         return xr.Dataset({
