0bc8b303039d8abea115c8c76bf30e1933852fe9
Conrad Stansbury <chstansbury@gmail.com>
2018-11-17 13:52:35 -0800

    Include pocket and gap analysis

--- arpes/analysis/__init__.py
+++ arpes/analysis/__init__.py
@@ -5,6 +5,8 @@ from .filters import *
 from .general import *
 from .mask import *
 from .tarpes import *
+from .pocket import *
+from .gap import *
 from .savitzky_golay import *
 from .xps import *
 from .kfermi import *
