9d291dfca2b75fc416500573f6565cf9336f989b
Daniel Eilbott <daniel.eilbott@berkeley.edu>
2019-02-17 17:34:28 -0800

    now accepts dataset and dataarray input

--- arpes/analysis/deconvolution.py
+++ arpes/analysis/deconvolution.py
@@ -1,4 +1,7 @@
 import numpy as np
+from arpes.typing import DataType
+from arpes.typing import xr_types
+from arpes.utilities import normalize_to_spectrum
 
 __all__ = ('deconvolve_ice',)
 
@@ -12,31 +15,42 @@ def _convolve(original_data, convolution_kernel):
     result = (convolved[n_offset:])[:n_points]
     return result
 
-def deconvolve_ice(spectrum,psf,n_iterations=5,deg=None):
-    """Deconvolve a spectrum by a given point spread function.
+def deconvolve_ice(data: DataType,psf,n_iterations=5,deg=None):
+    """Deconvolves data by a given point spread function.
     
-    :param spectrum -- numpy.ndarray:
+    :param data:
     :param psf:
     :param n_iterations -- the number of convolutions to use for the fit (default 5):
     :param deg -- the degree of the fitting polynominal (default n_iterations-3):
     :return numpy.ndarray:
     """
     
+    arr = normalize_to_spectrum(data)
+    if type(data) is np.ndarray:
+        pass
+    else:
+        arr = arr.values
+    
     if deg is None:
         deg = n_iterations - 3
     iteration_steps = list(range(1,n_iterations+1))
 
-    iteration_list = [spectrum]
+    iteration_list = [arr]
     color_list = np.linspace(0,0.9,n_iterations+1)[1:]
 
     for i in range(n_iterations-1):
         iteration_list.append(_convolve(iteration_list[-1],psf))
     iteration_list = np.asarray(iteration_list)
 
-    restoration = spectrum*0
+    deconv = arr*0
     for t, series in enumerate(iteration_list.T):
-        coefs = np.polyfit(iteration_steps,series,deg=n_iterations-3)
+        coefs = np.polyfit(iteration_steps,series,deg=deg)
         poly = np.poly1d(coefs)
-        restoration[t] = poly(0)
-        
-    return restoration
+        deconv[t] = poly(0)
+    
+    if type(data) is np.ndarray:
+        result = deconv
+    else:
+        result = normalize_to_spectrum(data).copy(deep=True)
+        result.values = deconv
+    return result
