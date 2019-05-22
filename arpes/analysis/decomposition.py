cc2fdd62ffd3ac656a49ee92b75e773f2b0eef8e
Conrad Stansbury <chstansbury@gmail.com>
2019-05-22 11:46:38 -0700

    Add covariance/correlation option to PCA
    Add a pipeline in order to support StandardScaler
    or another preprocessor.

--- arpes/analysis/decomposition.py
+++ arpes/analysis/decomposition.py
@@ -6,15 +6,23 @@ from arpes.utilities import normalize_to_spectrum
 __all__ = ('decomposition_along', 'pca_along', 'ica_along',)
 
 
-def decomposition_along(data: DataType, axes: List[str], decomposition_cls, **kwargs):
+def decomposition_along(data: DataType, axes: List[str], decomposition_cls, correlation=False, **kwargs):
+    from sklearn.pipeline import make_pipeline
+    from sklearn.preprocessing import StandardScaler
     flattened_data = normalize_to_spectrum(data).stack(fit_axis=axes)
 
     if len(flattened_data.dims) != 2:
         raise ValueError('Inappropriate number of dimensions after flattening: [{}]'.format(
             flattened_data.dims))
 
-    decomp = decomposition_cls(**kwargs)
-    decomp.fit(flattened_data.values.T)
+    if correlation:
+        pipeline = make_pipeline(StandardScaler(), decomposition_cls(**kwargs))
+    else:
+        pipeline = make_pipeline(decomposition_cls(**kwargs))
+
+    pipeline.fit(flattened_data.values.T)
+
+    decomp = pipeline.steps[-1][1]
 
     transform = decomp.transform(flattened_data.values.T)
 
@@ -22,9 +30,10 @@ def decomposition_along(data: DataType, axes: List[str], decomposition_cls, **kw
     into_first = into.dims[0]
     into = into.isel(**dict([[into_first, slice(0, transform.shape[1])]]))
     into = into.rename(dict([[into_first, 'components']]))
+
     into.values = transform.T
 
-    return into.unstack('fit_axis')
+    return into.unstack('fit_axis'), decomp
 
 
 def pca_along(*args, **kwargs):
