d502e97901ab48ae1cd2e678535ba3a6915a2f2e
Conrad Stansbury <chstansbury@gmail.com>
2019-08-07 16:12:48 -0700

    Add post-package conda tests

---
+++ conda/run_test.py
@@ -0,0 +1,23 @@
+"""
+Performs some basic sanity checks in order to make sure that the conda-build didn't
+go abysmally wrong. This seems to happen occasionally for configuration reasons I
+don't entirely understand.
+"""
+
+
+def check_imports():
+    import arpes
+    arpes.check()
+
+
+def check_load_example_data():
+    import arpes.io
+    import xarray as xr
+
+    data = arpes.io.load_example_data()
+    assert isinstance(data, xr.Dataset)
+
+
+if __name__ == '__main__':
+    check_imports()
+    check_load_example_data()
