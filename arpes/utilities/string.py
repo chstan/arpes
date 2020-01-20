8820760c0e462a3057a6ff69184f414814cdf553
Conrad Stansbury <chstansbury@gmail.com>
2020-01-20 11:58:04 -0800

    Add fallback methods for string decode since Igor seems to inconsistently use a single encoding

--- arpes/utilities/string.py
+++ arpes/utilities/string.py
@@ -1,8 +1,23 @@
 import re
 
-__all__ = ('snake_case',)
+__all__ = ('snake_case', 'safe_decode',)
 
 def snake_case(input: str):
     s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', input)
     s2 = re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
     return s2.replace('(', '').replace(')', '').replace(' ', '_').replace('/', '_').replace('__', '_')
+
+
+def safe_decode(input: bytes, prefer: str = None) -> str:
+    codecs = ['utf-8', 'latin-1', 'ascii']
+
+    if prefer:
+        codecs = [prefer] + codecs
+
+    for codec in codecs:
+        try:
+            return input.decode(codec)
+        except UnicodeDecodeError:
+            pass
+
+    input.decode('utf-8') # COULD NOT DETERMINE CODEC, RAISE
