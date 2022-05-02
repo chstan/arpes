a8805cf42fca4860a8157f04142fd7d050b189ea
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2022-05-02 20:49:40 +0900

    As the first step, change dependency

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ DESCRIPTION = "Modular data analysis code for angle resolved photoemission spect
 URL = "https://gitlab.com/lanzara-group/python-arpes"
 EMAIL = "chstan@berkeley.edu"
 AUTHOR = "Conrad Stansbury"
-REQUIRES_PYTHON = ">=3.8.0,<3.9"  # we're being less permissive because of pyqtgraph
+#REQUIRES_PYTHON = ">=3.8.0,<3.9"  # we're being less permissive because of pyqtgraph
+REQUIRES_PYTHON = ">=3.8.0"  # we're being less permissive because of pyqtgraph <- check it!!
 
 about = {}
 with open("./arpes/__init__.py") as fp:
@@ -27,7 +28,7 @@ DEPENDENCY_GROUPS = {
         "xarray>=0.16.1",
         "h5py>=3.2.1",
         "pyqtgraph>=0.12.0,<0.13.0",
-        "PyQt5==5.15",
+        "PyQt6>=6.3",
         "netCDF4>=1.5.0,<2.0.0",
         "colorcet",
         "pint",
@@ -91,13 +92,13 @@ Documentation available at: {}
 You should follow standard best practices for working with IPython and Jupyter.
 
 To get the interactive volumetric data explorer `qt_tool` you will need to install
-`PyQt5` and `pyqtgraph`. 
+`PyQt6` and `pyqtgraph`.
 
-To use the Igor data loading libraries in PyARPES you will need to install the `igor` 
+To use the Igor data loading libraries in PyARPES you will need to install the `igor`
 module from 'https://github.com/chstan/igorpy/tarball/712a4c4#egg=igor-0.3.1'.
 
-Some functionality, including PCA/Factor Analysis decomposition tools, require 
-additional heavy dependencies such as `scikit-learn` and `scikit-image`. 
+Some functionality, including PCA/Factor Analysis decomposition tools, require
+additional heavy dependencies such as `scikit-learn` and `scikit-image`.
 
 For Jupyter integration, please have a look at the documentation (link above).
 For support issues, contact chstansbury@gmail.com or chstan@berkeley.edu.
